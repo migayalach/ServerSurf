d18319b68a1644e543f4b282ffe5101dc79fdb1c
migayalach <ayalachavezmiguel@gmail.com>
2024-01-07 17:12:14 -0400

    se creo la estructura del servidor, base de datos, rutas y configuracion de los middlewares

---
+++ server/src/dataBase/dataBase.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false,
+    native: false,
+  }
+);
+
+module.exports = { sequelize, ...sequelize.models };
---
+++ server/src/routes/index.js
@@ -0,0 +1,4 @@
+const { Router } = require("express");
+const mainRouter = Router();
+
+module.exports = mainRouter;
