a9f1351c4ad0450fbba2515d282aa8a504ea93e8
migayalach <ayalachavezmiguel@gmail.com>
2024-01-07 23:13:51 -0400

    modelo entidad relacion

--- server/src/dataBase/dataBase.js
+++ server/src/dataBase/dataBase.js
@@ -1,6 +1,19 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+//TABLAS INTERMEDIAS
+const userSaleModel = require("../models/UserSale");
+const detailedSaleModel = require("../models/DetailedSale");
+const favoriteModel = require("../models/Favorites");
+const salesCartModel = require("../models/SalesCart");
+
+// TABLAS
+const levelModel = require("../models/Level");
+const userModel = require("../models/User");
+const saleModel = require("../models/Sale");
+const productModel = require("../models/Product");
+const categoryModel = require("../models/Category");
+
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
@@ -10,5 +23,42 @@ const sequelize = new Sequelize(
     native: false,
   }
 );
+
+levelModel(sequelize);
+userModel(sequelize);
+saleModel(sequelize);
+productModel(sequelize);
+userSaleModel(sequelize);
+detailedSaleModel(sequelize);
+categoryModel(sequelize);
+favoriteModel(sequelize);
+salesCartModel(sequelize);
+
+const {
+  Level,
+  User,
+  Sale,
+  UserSale,
+  Product,
+  DetailSale,
+  Category,
+  Favorites,
+  SalesCart,
+} = sequelize.models;
+
+Level.hasMany(User, { foreignKey: "levelId" });
+
+User.belongsToMany(Sale, { through: UserSale, foreignKey: "userId" });
+Sale.belongsToMany(User, { through: UserSale, foreignKey: "saleId" });
+
+Sale.belongsToMany(Product, { through: DetailSale, foreignKey: "saleId" });
+Product.belongsToMany(Sale, { through: DetailSale, foreignKey: "productId" });
+
+Category.hasMany(Product, { foreignKey: "categoryId" });
+
+User.belongsToMany(Product, { through: Favorites, foreignKey: "userId" });
+Product.belongsToMany(User, { through: Favorites, foreignKey: "productId" });
+
+
 
 module.exports = { sequelize, ...sequelize.models };
