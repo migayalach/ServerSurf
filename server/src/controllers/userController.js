c68a0f77eedcb9e4dc1fa0a2750a39f3acaa7c24
migayalach <ayalachavezmiguel@gmail.com>
2024-01-08 16:27:51 -0400

    controlador que guarda lso datos introducidos y que no haya repetidos, listo, falta encriptar la contraseña

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,32 +1,60 @@
-const createUser = (name) => {
-    return name;
+const { Op } = require("sequelize");
+const { Level, User } = require("../dataBase/dataBase");
+
+const createUser = async (levelId, nameUser, emailUser, user, password) => {
+  const existUser = await Level.findOne({
+    where: {
+      idLevel: levelId,
+    },
+  });
+  if (!existUser) {
+    throw Error(`El nivel que intenta asignar no se encuentra registrado`);
+  }
+  const existEmail = await User.findOne({
+    where: {
+      emailUser,
+    },
+  });
+  if (existEmail) {
+    throw Error(`Lo siento no puede haber dos cuentas con la misma dirección email`);
+  }
+
+  const newUser = await User.create({
+    nameUser,
+    emailUser,
+    user,
+    password,
+    levelId,
+  });
+
+  return newUser;
 };
 
 const getNameUser = (name) => {
-    return name;
+  return name;
 };
 
 const getUserId = (id) => {
-    return id;
+  return id;
 };
 
 const getAllUser = () => {
-    return 'Todos los users';
+  return "Todos los users";
 };
 
 const userDelete = (id) => {
-    return 'Elimina el user ' + id;
+  return "Elimina el user " + id;
 };
 
 const userUpDate = (id) => {
-    return 'Actualiza el user ' + id;
+  return "Actualiza el user " + id;
 };
 
 module.exports = {
-    createUser,
-    getNameUser,
-    getUserId,
-    getAllUser,
-    userDelete,
-    userUpDate
+  createUser,
+  getNameUser,
+  getUserId,
+  getAllUser,
+  userDelete,
+  userUpDate,
 };
