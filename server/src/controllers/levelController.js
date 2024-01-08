6eed58e944e06d40bfd7276f4b1e1503fa939e4f
Matias Reyna <matiireyna@hotmail.com>
2024-01-08 11:34:35 -0300

    Inicio de handlers y contollers

--- server/src/controllers/levelController.js
+++ server/src/controllers/levelController.js
@@ -2,4 +2,21 @@ const createLevel = (name) => {
   return name;
 };
 
-module.exports = { createLevel };
+const getNameLevel = (name) => {
+  return name;
+};
+
+const getLevelId = (id) => {
+  return id;
+};
+
+const getAllLevel = () => {
+  return 'TODOS LOS NIVELES';
+};
+
+module.exports = { 
+  createLevel,
+  getNameLevel,
+  getLevelId,
+  getAllLevel
+};
--- server/src/handlers/levelHandler.js
+++ server/src/handlers/levelHandler.js
@@ -1,4 +1,4 @@
-const { createLevel } = require("../controllers/levelController");
+const { createLevel, getNameLevel, getLevelId, getAllLevel } = require("../controllers/levelController");
 
 const postLevel = (request, response) => {
   const { name } = request.body;
@@ -10,6 +10,35 @@ const postLevel = (request, response) => {
   }
 };
 
+const getLevelName = (request, response) => {
+  const { name } = request.query;
+  try {
+    if (name) {
+      const userName = getNameLevel(name);
+      response.status(200).json(userName);
+    } else {
+      const allUser = getAllLevel();
+      response.status(200).json(allUser);
+    } 
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+const getIdLevel = (request, response) => {
+  const { id } = request.params;
+  try {
+    const userFind = getLevelId(id);
+    response.status(200).json(userFind);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+const deleteUser = (request, response) => {};
+
 module.exports = {
   postLevel,
+  getLevelName,
+  getIdLevel
 };
