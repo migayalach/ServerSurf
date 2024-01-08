6e9032484b5d7249da62ac82752fa67c6f792cad
migayalach <ayalachavezmiguel@gmail.com>
2024-01-08 16:28:46 -0400

    handler con manejador de errores si es que existieran listo, solo falta arreglar las respuestas

--- server/src/handlers/userHandler.js
+++ server/src/handlers/userHandler.js
@@ -1,64 +1,77 @@
-const { createUser, getNameUser, getAllUser, getUserId, userDelete, userUpDate } = require("../controllers/userController");
+const {
+  createUser,
+  getNameUser,
+  getAllUser,
+  getUserId,
+  userDelete,
+  userUpDate,
+} = require("../controllers/userController");
 
-const postUser = (request, response) => {
-    const { name } = request.body;
-    try {
-       const newUser = createUser(name);
-       response.status(200).json({ create: true, newUser }); 
-    } catch (error) {
-        response.status(400).json({ error: error.message });
-    }
+const postUser = async (request, response) => {
+  const { levelId, nameUser, emailUser, user, password } = request.body;
+  try {
+    const newUser = await createUser(
+      levelId,
+      nameUser,
+      emailUser,
+      user,
+      password
+    );
+    response.status(200).json({ create: true, newUser });
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 };
 
 const getUserName = (request, response) => {
-    const { name } = request.query;
-    try {
-        if (name) {
-            const userName = getNameUser(name);
-            response.status(200).json(userName)
-        } else {
-            const allUsers = getAllUser();
-            response.status(200).json(allUsers);
-        }
-    } catch (error) {
-        response.status(400).json({ error: error.message });
+  const { name } = request.query;
+  try {
+    if (name) {
+      const userName = getNameUser(name);
+      response.status(200).json(userName);
+    } else {
+      const allUsers = getAllUser();
+      response.status(200).json(allUsers);
     }
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 };
 
 const getIdUser = (request, response) => {
-    const { id } = request.params;
-    try {
-        const userFind = getUserId(id);
-        response.status(200).json(userFind);
-    } catch (error) {
-        response.status(400).json({ error: error.message });
-    }
+  const { id } = request.params;
+  try {
+    const userFind = getUserId(id);
+    response.status(200).json(userFind);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 };
 
 const deleteUser = (request, response) => {
-    const { id } = request.params;
-    try {
-        const userFind = userDelete(id);
-        response.status(200).json(userFind);
-    } catch (error) {
-        response.status(400).json({ error: error.message }); 
-    }
+  const { id } = request.params;
+  try {
+    const userFind = userDelete(id);
+    response.status(200).json(userFind);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 };
 
 const userPut = (request, response) => {
-    const { id } = request.body;
-    try {
-        const upDate = userUpDate(id);
-        response.status(200).json(upDate);
-    } catch (error) {
-        response.status(400).json({ error: error.message });   
-    }
+  const { id } = request.body;
+  try {
+    const upDate = userUpDate(id);
+    response.status(200).json(upDate);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
-    postUser,
-    getUserName,
-    getIdUser,
-    deleteUser,
-    userPut
+  postUser,
+  getUserName,
+  getIdUser,
+  deleteUser,
+  userPut,
 };
