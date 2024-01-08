0ef5eee847ff00a0722e8e5c4a0cf97748e14a0a
migayalach <ayalachavezmiguel@gmail.com>
2024-01-08 15:59:40 -0400

    se realizo la creacion del nombre del nivel de acceso

--- server/src/routes/levelRouter.js
+++ server/src/routes/levelRouter.js
@@ -4,13 +4,10 @@ const { postLevel, getLevelName, getIdLevel, deleteLevel, levelPut } = require("
 
 const levelRouter = Router();
 
-levelRouter.post("/", postLevel);
-
-levelRouter.get("/", getLevelName);
+levelRouter.post("/", postLevel);   //oK
+levelRouter.get("/", getLevelName); //ok all, falta por name
 levelRouter.get("/:id", getIdLevel);
-
 levelRouter.delete("/:id", deleteLevel);
-
 levelRouter.put("/", levelPut);
 
 module.exports = levelRouter;
