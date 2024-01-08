ede2327ca39303f19528bbf72be10a8430589a3c
migayalach <ayalachavezmiguel@gmail.com>
2024-01-08 16:01:58 -0400

    limpieza

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const mainRouter = Router();
 
 // route's
-const levelRouter = require("./levelRouter"); //MATY
+const levelRouter = require("./levelRouter");
 const userRouter = require("./userRouter");
 const cartRouter = require("./cartRouter");
 const saleRouter = require("./saleRouter");
@@ -13,19 +13,14 @@ const detailRouter = require("./detailRouter");
 const commentRouter = require("./commentRouter");
 
 // entry points
-mainRouter.use("/level", levelRouter); //MATY
-mainRouter.use("/user", userRouter); //MATY
-mainRouter.use("/cart", cartRouter); //MATY
-mainRouter.use("/sale", saleRouter); //MARIANA
-mainRouter.use("/product", productRouter); //MARIANA
-mainRouter.use("/favorite", favoriteRouter); //MELANI
-mainRouter.use("/category", categoryRouter); //MELANI
+mainRouter.use("/level", levelRouter);
+mainRouter.use("/user", userRouter);
+mainRouter.use("/cart", cartRouter);
+mainRouter.use("/sale", saleRouter);
+mainRouter.use("/product", productRouter);
+mainRouter.use("/favorite", favoriteRouter);
+mainRouter.use("/category", categoryRouter);
 mainRouter.use("/detail", detailRouter);
 mainRouter.use("/comment", commentRouter);
 
 module.exports = mainRouter;
-
-// busqueda por id, name, todo (name)  - GET
-// agregar (name) - POST
-// eliminar (name) - DELETE
-// editar (name) - UPDATE
