2ae895374b3ba9cac640587f14264f442639c43f
migayalach <ayalachavezmiguel@gmail.com>
2024-01-09 03:38:25 -0400

    manejo de errores para category, listo, solo falta limpiar respuestas y devolver armados los json

--- server/src/handlers/categoryHandler.js
+++ server/src/handlers/categoryHandler.js
@@ -1,35 +1,40 @@
-const { allCategory, createCategory, deleteCat } = require("../controllers/categoryController");
+const {
+  allCategory,
+  createCategory,
+  deleteCat,
+} = require("../controllers/categoryController");
 
-const getCategory = (req, res) => {
-    try {
-        const response = allCategory();
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+const getCategory = async (request, response) => {
+  try {
+    const getAllCategory = await allCategory();
+    response.status(200).json(getAllCategory);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 };
 
-const postCategory = (req, res) => {
-    const newCategory = req.body;
-    try {
-        const response = createCategory(newCategory);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+const postCategory = async (request, response) => {
+  const { nameCategory } = request.body;
+  try {
+    const newCategory = await createCategory(nameCategory);
+    response.status(200).json(newCategory);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 };
 
 const deleteCategory = (req, res) => {
-    const { id } = req.params;
-    try {
-        const response = deleteCat(id);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  const { id } = req.params;
+  try {
+    const response = deleteCat(id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
+
 module.exports = {
-    getCategory,
-    postCategory,
-    deleteCategory
+  getCategory,
+  postCategory,
+  deleteCategory,
 };
