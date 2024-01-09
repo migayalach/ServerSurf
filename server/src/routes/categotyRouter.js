8a7165da1e13471f01f1f0ae89f2201c45e8bea2
migayalach <ayalachavezmiguel@gmail.com>
2024-01-09 03:32:38 -0400

    se llego hacer los entry point's getAll y post

--- server/src/routes/categotyRouter.js
+++ server/src/routes/categotyRouter.js
@@ -1,13 +1,17 @@
 const { Router } = require("express");
 
-const { getCategory, postCategory, deleteCategory } = require("../handlers/categoryHandler");
+const {
+  getCategory,
+  postCategory,
+  deleteCategory,
+} = require("../handlers/categoryHandler");
 
 const categoryRouter = Router();
 
-categoryRouter.get("/", getCategory);
-
-categoryRouter.post("/create", postCategory);
-
+categoryRouter.get("/", getCategory); //ok
+categoryRouter.post("/", postCategory); //ok
 categoryRouter.delete("/:id", deleteCategory);
+//FALTA EDITAR
+//MOSTRAR por id, name
 
 module.exports = categoryRouter;
