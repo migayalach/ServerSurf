5f084626556734fcda733179d2d4669ed38c28f3
migayalach <ayalachavezmiguel@gmail.com>
2024-01-09 03:44:12 -0400

    se agrega datos mediante create y tambien se muestra todo lo que hay en los registros

--- server/src/controllers/categoryController.js
+++ server/src/controllers/categoryController.js
@@ -1,18 +1,33 @@
+const { Op } = require("sequelize");
+const { Category } = require("../dataBase/dataBase");
 
-const allCategory = () => {
-    return category;
+const allCategory = async () => {
+  return await Category.findAll();
 };
 
-const createCategory = (newCategory) => {
-    return newCategory;
+const createCategory = async (nameCategory) => {
+  const categoryExist = await Category.findOne({
+    where: {
+      nameCategory: {
+        [Op.iLike]: `%${nameCategory}%`,
+      },
+    },
+  });
+
+  if (categoryExist) {
+    throw Error(`Ya existe esta categoria, no puede haber duplicados`);
+  }
+
+  await Category.create({ nameCategory });
+  return `Categoria: ${nameCategory} creada con exito`;
 };
 
 const deleteCat = (id) => {
-    return `se borro la categoria ${id}`;
+  return `se borro la categoria ${id}`;
 };
 
 module.exports = {
-    allCategory,
-    createCategory,
-    deleteCat
+  allCategory,
+  createCategory,
+  deleteCat,
 };
