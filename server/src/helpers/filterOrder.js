e79a465ed76fc2cf993dd151fbfcf6fd361e20e2
migayalach <ayalachavezmiguel@gmail.com>
2024-01-12 16:15:38 -0400

    se ordena por orden puede ser ASC o DESC

--- server/src/helpers/filterOrder.js
+++ server/src/helpers/filterOrder.js
@@ -1,21 +1,25 @@
-// const orderProduct = (array, search) => {
-//   let n = array.length,
-//     aux = 0,
-//     temp = 0;
-//   for (let i = 0; i < n - 1; i++) {
-//     //   aux = i;
-//     //   for (let j = i + 1; j < n; j++) {
-//     //     let valor1 = valor(array[j][key]);
-//     //     let valor2 = valor(array[aux][key]);
-//     //     if (valor1 < valor2 && order === "ASC") aux = j;
-//     //     if (valor1 > valor2 && order === "DESC") aux = j;
-//     //   }
-//     //   temp = array[aux];
-//     //   array[aux] = array[i];
-//     //   array[i] = temp;
-//   }
-//   return array;
-// };
+const valor = (text) => {
+  let character = text.split("")[0];
+  return character.charCodeAt();
+};
+const orderByASCDESC = (array, search) => {
+  let n = array.length,
+    aux = 0,
+    temp = 0;
+  for (let i = 0; i < n - 1; i++) {
+    aux = i;
+    for (let j = i + 1; j < n; j++) {
+      let valor1 = valor(array[j].name);
+      let valor2 = valor(array[aux].name);
+      if (valor1 < valor2 && search === "ASC") aux = j;
+      if (valor1 > valor2 && search === "DESC") aux = j;
+    }
+    temp = array[aux];
+    array[aux] = array[i];
+    array[i] = temp;
+  }
+  return array;
+};
 
 const orderProduct = (
   array,
@@ -25,13 +29,13 @@ const orderProduct = (
   priceEnd,
   stockBy
 ) => {
+  // ORDEN ASCENDENTE O DESCENDENTE POR NOMBRE
+  if (orderBy) {
+    return orderByASCDESC(array, orderBy);
+  }
+
   const aux = array.filter((data) => data.nameCategory == nameCategory);
   return aux;
-};
-
-const valor = (text) => {
-  let character = text.split("")[0];
-  return character.charCodeAt();
 };
 
 const filterProducts = (cardsUser, gender) =>
