21bc6a12d6b4796f11f9fb8addb4014b731e4206
migayalach <ayalachavezmiguel@gmail.com>
2024-01-12 03:26:30 -0400

    se implemento promiseAll para poder mostrar todos los datos limpios y estos sean consumidos deacuerdo al frontend

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,6 +1,62 @@
 const { Op } = require("sequelize");
 const { Product, Category } = require("../dataBase/dataBase");
 
+const clearObj = (obj, nameCategory) => {
+  return {
+    idProduct: obj.idProduct,
+    idCategory: obj.idCategory,
+    nameCategory,
+    code: obj.code,
+    name: obj.name,
+    type: obj.type,
+    image: obj.image,
+    characteristics: obj.characteristics,
+    priceProduct: obj.priceProduct,
+    stock: obj.stock,
+    description: obj.description,
+  };
+};
+
+const listProductsPromisse = async (productData) => {
+  let allProducts = null;
+  if (productData === undefined) {
+    allProducts = await getAllProducts();
+  } else {
+    allProducts = productData;
+  }
+
+  const promisse = allProducts.map(
+    async ({
+      idProduct,
+      idCategory,
+      code,
+      name,
+      type,
+      image,
+      characteristics,
+      priceProduct,
+      stock,
+      description,
+    }) => {
+      const { nameCategory } = await Category.findByPk(idCategory);
+      const obj = {
+        idProduct,
+        idCategory,
+        code,
+        name,
+        type,
+        image,
+        characteristics,
+        priceProduct,
+        stock,
+        description,
+      };
+      return clearObj(obj, nameCategory);
+    }
+  );
+  return await Promise.all(promisse);
+};
+
 const createProduct = async (
   idCategory,
   code,
@@ -9,19 +65,25 @@ const createProduct = async (
   image,
   characteristics,
   priceProduct,
+  stock,
   description
 ) => {
-  const existCategory = await Category.findOne({ where: { idCategory } });
+  const existCategory = await Category.findOne(
+    { where: { idCategory } },
+    { attribute: [`nameCategory`] }
+  );
+
   if (!existCategory) {
     throw Error`La categoria que intenta asignar no se encuentra registrada`;
   }
 
   const existCode = await Product.findOne({ where: { code } });
+
   if (existCode) {
     throw Error`El codigo: ${code} que introdujo ya esta registrado`;
   }
 
-  const newProduct = await Product.create({
+  let newProduct = await Product.create({
     idCategory,
     code,
     name,
@@ -29,9 +91,15 @@ const createProduct = async (
     image,
     characteristics,
     priceProduct,
+    stock,
     description,
   });
-  return newProduct;
+
+  return {
+    message: `Producto: '${newProduct.name}', creado con exito`,
+    dataNewProduct: clearObj(newProduct, existCategory.nameCategory),
+    listProducts: await listProductsPromisse(),
+  };
 };
 
 const getProductName = async (name, code) => {
@@ -42,7 +110,10 @@ const getProductName = async (name, code) => {
     if (!productData.length) {
       throw Error`No se pudo encontrar nada`;
     }
-    return productData;
+    return {
+      message: `Lista de productos`,
+      productData: await listProductsPromisse(productData),
+    };
   } else if (code) {
     const productData = await Product.findAll({
       where: { code: { [Op.iLike]: `%${code}%` } },
@@ -50,24 +121,37 @@ const getProductName = async (name, code) => {
     if (!productData.length) {
       throw Error`No se pudo encontrar nada`;
     }
-    return productData;
+    return {
+      message: `Lista de productos`,
+      productData: await listProductsPromisse(productData),
+    };
   }
 };
 
 const getProductId = async (idProduct) => {
   if (idProduct) {
-    const existingProduct = await Product.findOne({ where: { idProduct } });
-
-    if (!existingProduct) {
-      throw Error(`Producto ${idProduct} no encontrado`);
-    } else {
-      return existingProduct;
+    const listProducts = await Product.findOne({ where: { idProduct } });
+    if (!listProducts) {
+      throw Error(`El producto que busca no pudo ser encontrado`);
     }
+    const existCategory = await Category.findOne(
+      { where: { idCategory: listProducts.idCategory } },
+      { attribute: [`nameCategory`] }
+    );
+    return {
+      message: `Producto encontrado con exito`,
+      product: clearObj(listProducts, existCategory.nameCategory),
+    };
   }
 };
 
-const getAllProducts = async () => {
-  return await Product.findAll();
+const getAllProducts = async (flag) => {
+  if (flag) {
+    return await listProductsPromisse();
+  }
+  return await Product.findAll({
+    order: [["idProduct", "ASC"]],
+  });
 };
 
 const putProduct = async (
@@ -79,42 +163,62 @@ const putProduct = async (
   image,
   characteristics,
   priceProduct,
+  stock,
   description
 ) => {
   const existingProduct = await Product.findOne({ where: { idProduct } });
 
   if (!existingProduct) {
-    throw Error(`Producto ${idProduct} no encontrado`);
+    throw Error(`Producto no encontrado`);
   }
 
-  const result = await Product.update({
-    idCategory,
-    code,
-    name,
-    type,
-    image,
-    characteristics,
-    priceProduct,
-    description,
-  }, { where: { idProduct }});
+  const result = await Product.update(
+    {
+      idCategory,
+      code,
+      name,
+      type,
+      image,
+      characteristics,
+      priceProduct,
+      stock,
+      description,
+    },
+    { where: { idProduct } }
+  );
 
-  if(result){
-    return `Información de producto ${idProduct} actualizada con éxito`
+  if (result) {
+    return {
+      message: `Producto actualizado con exito`,
+      dataUpdateProduct: await getProductId(idProduct),
+      listProducts: await listProductsPromisse(),
+    };
   } else {
-    throw Error (`No se pudo actualizar la información del producto ${idProduct}`)
+    throw Error(
+      `No se pudo actualizar la información del producto ${idProduct}`
+    );
   }
 };
 
 const deleteProduct = async (idProduct) => {
   const existingProduct = await Product.findOne({ where: { idProduct } });
-
   if (!existingProduct) {
-    throw Error(`No exíste el producto ${idProduct}`);
+    throw Error(`El producto que desea eliminar no existe`);
   }
+
+  const existCategory = await Category.findOne(
+    { where: { idCategory: existingProduct.idCategory } },
+    { attribute: [`nameCategory`] }
+  );
+
   const result = await Product.destroy({ where: { idProduct } });
 
   if (result) {
-    return `Producto ${idProduct} borrado con éxito`;
+    return {
+      message: `Producto eliminado con exito `,
+      dataDeleteProduct: clearObj(existingProduct, existCategory.nameCategory),
+      listProducts: await listProductsPromisse(),
+    };
   } else {
     throw Error(`No se pudo borrar el producto ${idProduct}`);
   }
