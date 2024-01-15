f0ed95790187fef7cce323b723e16ffddd7ca92c
Mariana <mariana.crt@gmail.com>
2024-01-14 22:39:49 -0800

    handler, controller y rutas de brand

--- server/src/routes/brandRouter.js
+++ server/src/routes/brandRouter.js
@@ -1,5 +1,19 @@
 const { Router } = require("express");
 
+const {
+    postBrand,
+    getBrandByName,
+    getBrandById,
+    deleteBrand,
+    upDateBrand
+} = require('../handlers/brandHandler')
+
 const brandRouter = Router();
+
+brandRouter.post("/", postBrand); //OK.
+brandRouter.get("/", getBrandByName); //OK.
+brandRouter.get("/:idBrand", getBrandById); //OK.
+brandRouter.delete("/:idBrand", deleteBrand); //OK.
+brandRouter.put("/", upDateBrand) //OK.
 
 module.exports = brandRouter;
