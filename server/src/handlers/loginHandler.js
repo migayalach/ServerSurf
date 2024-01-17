2078835d816dafc2f16a0aca0d1f707ff180fc98
migayalach <ayalachavezmiguel@gmail.com>
2024-01-17 02:04:34 -0400

    login

--- server/src/handlers/loginHandler.js
+++ server/src/handlers/loginHandler.js
@@ -3,8 +3,13 @@ const userAccess = require("../controllers/loginController");
 const postLogin = async (request, response) => {
   const { email, password } = request.body;
   try {
-    const { access, message, idUser, name } = await userAccess(email, password);
-    response.status(200).json({ access, message, idUser, name });
+    const { access, idLevel, idUser, name, level, message } = await userAccess(
+      email,
+      password
+    );
+    response
+      .status(200)
+      .json({ access, idLevel, idUser, name, level, message });
   } catch (error) {
     response.status(400).json({ access: false, message: error.message });
   }
