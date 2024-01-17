427eaefa06f5b37d8dfa5b703b1993087636b63f
migayalach <ayalachavezmiguel@gmail.com>
2024-01-17 01:40:14 -0400

    helpers

---
+++ server/src/controllers/helperControllers.js
@@ -0,0 +1,79 @@
+const { Product, Category, Brand, Color } = require("../dataBase/dataBase");
+const { clearObj } = require("../helpers/dataClear");
+
+async function categoryExist(idCategory) {
+  return await Category.findOne(
+    { where: { idCategory } },
+    { attribute: [`nameCategory`] }
+  );
+}
+
+async function colorExist(idColor) {
+  return await Color.findOne(
+    { where: { idColor } },
+    { attribute: [`nameColor`] }
+  );
+}
+
+async function brandExist(idBrand) {
+  return await Brand.findOne(
+    { where: { idBrand } },
+    { attribute: [`brandName`] }
+  );
+}
+
+async function productExist(code) {
+  return await Product.findOne({ where: { code } });
+}
+
+async function productIdExist(idProduct) {
+  return await Product.findOne({ where: { idProduct } });
+}
+
+const listProductsPromisse = async (productData) => {
+  const promisse = productData.map(
+    async ({
+      idProduct,
+      idCategory,
+      idColor,
+      idBrand,
+      code,
+      name,
+      type,
+      image,
+      characteristics,
+      priceProduct,
+      stock,
+      description,
+    }) => {
+      const { nameCategory } = await Category.findByPk(idCategory);
+      const { brandName } = await Brand.findByPk(idBrand);
+      const { nameColor } = await Color.findByPk(idColor);
+      const obj = {
+        idProduct,
+        idCategory,
+        idColor,
+        idBrand,
+        code,
+        name,
+        type,
+        image,
+        characteristics,
+        priceProduct,
+        stock,
+        description,
+      };
+      return clearObj(obj, nameCategory, brandName, nameColor);
+    }
+  );
+  return await Promise.all(promisse);
+};
+
+module.exports = {
+  categoryExist,
+  colorExist,
+  brandExist,
+  productExist,
+  productIdExist,
+  listProductsPromisse,
+};
