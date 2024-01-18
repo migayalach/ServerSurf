5015263960d005048456f6bc3bbe1c778aad40b7
migayalach <ayalachavezmiguel@gmail.com>
2024-01-17 22:30:32 -0400

    para detalle solo hay dos entry points disponibles, los cuales son la creacion donde se agrega todos los productos, la cantidad y el idSale que corresponde a la venta creada

--- server/src/routes/detailRouter.js
+++ server/src/routes/detailRouter.js
@@ -11,10 +11,12 @@ const {
 const detailRouter = Router();
 
 // ENTRY POIN'S
+detailRouter.get("/:idSale", getIdDetail);
 detailRouter.post("/", postDetail);
+
+// NO SE NECESITA ESTO PORQUE UN DETALLE NO PUEDE SER EDITADO, ELIMINADO, TRAER TODO 
 detailRouter.put("/", updateDetail);
 detailRouter.delete("/:idDetail", deleteDetail);
-detailRouter.get("/:idDetail", getIdDetail);
 detailRouter.get("/", getDetailAll);
 
 module.exports = detailRouter;
