5aae2f955781b11ebf24ee9be99c8beb42c63a29
MIGUELAYALACHAVEZ <89620485+MIGUELAYALACHAVEZ@users.noreply.github.com>
2024-01-31 02:34:56 -0300

    se exporto el servidor que contiene a socketIO

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-const app = require("./src/app");
+const { app, server } = require("./src/app");
+const { sequelize } = require("./src/dataBase/dataBase");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-const { sequelize } = require("./src/dataBase/dataBase");
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   sequelize.sync({ alter: true });
   console.log(`Servidor levantado en el puerto: ${PORT}`);
 });
